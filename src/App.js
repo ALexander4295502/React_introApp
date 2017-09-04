3bf37deb42bd5cb686a667056b78e6a7832c56ea
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-04 13:47:42 -0500

    refactor jsx in react to stateless functional components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForms';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
@@ -11,19 +13,8 @@ class App extends Component {
           <h2>Welcome to React with Redux</h2>
         </div>
         <div className="Todo-App">
-          <form>
-            <input type="text"/>
-          </form>
-          <div className="Todo-List">
-            <ul>
-              {this.props.todos.map(todo => (
-                <li key={todo.id}>
-                  <input type="checkbox" defaultChecked={todo.isComplete}/>
-                  {todo.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+          <TodoForm />
+          <TodoList todos={this.props.todos}/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ const state = {
     {id: 2, name: 'Create Initial State', isComplete: true},
     {id: 3, name: 'Use State to Render UI', isComplete: false},
   ]
-}
+};
 ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
 registerServiceWorker();
