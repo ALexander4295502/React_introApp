478a0dceaa4d51a6fb6165d23afa945d207075a0
ALexander4295502 <yuanzhengstl@gmail.com>
2017-09-04 22:33:31 -0500

    deploy the app

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -38,7 +38,7 @@ export const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case 'active':
       return todos.filter(t => !t.isComplete);
-    case 'completed':
+    case 'complete':
       return todos.filter(t => t.isComplete);
     default:
       return todos;
